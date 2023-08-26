e90df64ff0cd21e890f4d1bbee28f83c2d15860d
himanshu1234b <72341928+himanshu1234b@users.noreply.github.com>
2023-08-26 23:23:48 +0530

    modified: index.html modified: style.css modified: main.js

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-$(".custom-carousel").owlCarousel({
-    autoWidth: true,
-    loop: true
-  });
-  $(document).ready(function () {
-    $(".custom-carousel .item").hover(function () {
-      $(".custom-carousel .item").not($(this)).removeClass("active");
-      $(this).toggleClass("active");
-    });
-  });
-  
+const scrollContent = document.querySelector('.scroll-content');
+
+scrollContent.addEventListener('mouseover', () => {
+  scrollContent.style.animationPlayState = 'paused';
+});
+
+scrollContent.addEventListener('mouseout', () => {
+  scrollContent.style.animationPlayState = 'running';
+});
+
+
+//  Right scroll JavaScript 
+
+const scrollContentRight = document.querySelector('.scroll-content-right');
+
+scrollContentRight.addEventListener('mouseover', () => {
+  scrollContentRight.style.animationPlayState = 'paused';
+});
+
+scrollContentRight.addEventListener('mouseout', () => {
+  scrollContentRight.style.animationPlayState = 'running';
+});
+
+// Cloud scroll section 
+
+const scrollCloudContent = document.querySelector('.scroll-cloud-content');
+
+scrollCloudContent.addEventListener('mouseover', () => {
+  scrollCloudContent.style.animationPlayState = 'paused';
+});
+
+scrollCloudContent.addEventListener('mouseout', () => {
+  scrollCloudContent.style.animationPlayState = 'running';
+});
